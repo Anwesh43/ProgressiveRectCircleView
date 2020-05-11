22a9e8b35123f74f3a31a6894ea52373354001ed
Anwesh43 <anweshthecool0@gmail.com>
2020-05-12 04:59:01 +0530

    adding constants and libraries

--- progressiverectcircleview/src/main/java/com/anwesh/uiprojects/progressiverectcircleview/ProgressiveRectCircleView.kt
+++ progressiverectcircleview/src/main/java/com/anwesh/uiprojects/progressiverectcircleview/ProgressiveRectCircleView.kt
@@ -4,3 +4,21 @@ package com.anwesh.uiprojects.progressiverectcircleview
  * Created by anweshmishra on 12/05/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val parts : Int = 3
+val arcs : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
