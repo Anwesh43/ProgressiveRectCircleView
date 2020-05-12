f28295baa5eb1dfc1c8b46afc24578a45c4caddd
Anwesh43 <anweshthecool0@gmail.com>
2020-05-12 05:57:55 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedprogressiverectcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedprogressiverectcircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedprogressiverectcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.progressiverectcircleview.ProgressiveRectCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ProgressiveRectCircleView.create(this)
     }
 }
